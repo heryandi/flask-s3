109e4e2b383f4b8f2575cd6a584530e8b4199f60
Heryandi <heryandi@gmail.com>
2013-07-16 23:25:29 +0800

    Add working CORS upload

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from datetime import datetime, timedelta
+import base64
+import hmac, hashlib
 
+from flask import Flask, json, jsonify, render_template, request
+
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+S3_BUCKET_NAME = ""
+S3_BUCKET_URL = "//" + S3_BUCKET_NAME + ".s3.amazonaws.com" 
+
+app = Flask(__name__)
 @app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == "POST":
-        file = request.files["img"]
-        file.save("/home/user/Desktop/test.jpg")
-        return render_template("index.html", success = "Successfully uploaded")
-    return render_template("index.html")
+    return render_template("index.html", AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID, S3_BUCKET_URL = S3_BUCKET_URL)
+
+@app.route("/s3signature", methods=["GET"])
+def s3signature():
+    filename = request.args.get("name")
+
+    policy_object = {
+        "expiration": (datetime.utcnow() + timedelta(weeks=1)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "conditions": [
+            { "bucket": S3_BUCKET_NAME },
+            { "acl": "public-read" },
+            [ "starts-with", "$key", "" ],
+            { "success_action_status": "200" },
+        ]
+    }
+    policy = base64.b64encode(json.dumps(policy_object).replace("\n", "").replace("\r", ""))
+    signature = base64.b64encode(hmac.new(AWS_SECRET_ACCESS_KEY, policy, hashlib.sha1).digest())
+
+    return jsonify({
+        "key": filename,
+        "policy": policy,
+        "signature": signature
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
